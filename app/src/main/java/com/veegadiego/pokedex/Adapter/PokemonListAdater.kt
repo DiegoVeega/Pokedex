1d2e4f087f7ee8d8b85cea3c3e04ca8215880986
DiegoVeega <00148816@uca.edu.sv>
2019-04-22 16:21:53 -0600

    Creacion de interfaz para manipular el click listener

--- app/src/main/java/com/veegadiego/pokedex/Adapter/PokemonListAdater.kt
+++ app/src/main/java/com/veegadiego/pokedex/Adapter/PokemonListAdater.kt
@@ -6,8 +6,10 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.veegadiego.pokedex.Interface.InterfaceItemClickListener
 import com.veegadiego.pokedex.Model.Pokemon
 import com.veegadiego.pokedex.R
 import kotlinx.android.synthetic.main.pokemon_list_item.view.*
@@ -28,15 +30,32 @@ class PokemonListAdater (internal var context: Context, internal var pokemonList
     override fun onBindViewHolder(holder: MiViewHolder, position: Int) {
         Glide.with(context).load(pokemonList[position].img).into(holder.pokemon_image)
         holder.pokemon_name.text = pokemonList[position].name
+
+        //SOBREESCRITURA DE METODOS DE INTERFAZ CLICK LISTENER
+        holder.setItemClickListener(object:InterfaceItemClickListener{
+            override fun onClick(view: View, position: Integer) {
+                Toast.makeText(context,"Click a pokemon" + pokemonList[position.toInt()].name,Toast.LENGTH_LONG).show()
+
+            }
+        })
     }
     ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
     inner class MiViewHolder(itemView: View):RecyclerView.ViewHolder(itemView){
         internal var pokemon_image:ImageView
         internal var pokemon_name:TextView
+
+        internal var itemClickListener:InterfaceItemClickListener?=null
+
+        fun setItemClickListener(IitemClickListener: InterfaceItemClickListener){
+            this.itemClickListener = IitemClickListener
+        }
+
         init {
             pokemon_image = itemView.findViewById(R.id.pokemon_image) as ImageView
             pokemon_name = itemView.findViewById(R.id.pokemon_name) as TextView
+
+            itemView.setOnClickListener{ view -> itemClickListener!!.onClick(view,adapterPosition)}
         }
     }
 
