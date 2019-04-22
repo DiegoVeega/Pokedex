4ceb10e8b9908eb6bf651f4163e826c870e3f8f9
DiegoVeega <00148816@uca.edu.sv>
2019-04-21 22:47:15 -0600

    Creacion de modelos para lso objetos Pokemon y creacion base de adaptador

---
+++ app/src/main/java/com/veegadiego/pokedex/Adapter/PokemonListAdater.kt
@@ -0,0 +1,6 @@
+package com.veegadiego.pokedex.Adapter
+
+class PokemonListAdater {
+
+
+}
---
+++ app/src/main/java/com/veegadiego/pokedex/Model/Pokedex.kt
@@ -0,0 +1,10 @@
+package com.veegadiego.pokedex.Model
+
+class Pokedex {
+
+    /*
+    public List<Pokemon> pokemon { get; set; }
+     */
+
+    var pokemon:List<Pokemon>? = null
+}
